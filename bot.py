9bbad789af12afa242858f517cd4435ef7663a61
Lucastexpiastri124567 <lucastexeirapiastri@gmail.com>
2025-01-08 18:43:27 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import discord
 from discord.ext import commands
-from Dado import Dado
+from Dado import lanzar_dado
 
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
-bot = commands.Bot(command_prefix='!', intents=intents)(intents=intents)
-
+bot = commands.Bot(command_prefix='?', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Hemos iniciado sesión como {bot.user}')
 
 @bot.command()
-async def Dado(ctx):
-    await ctx.send(Dado())
-    
-bot.run("MTMyNDU2NDYzMDY4ODg5NDk4Nw.GGrxMW.fVLoQVBFh0QpLQsjPTXVNyEYe_pP3D3I_kUYWo")
+async def dado(ctx):
+    resultado = lanzar_dado()  # Llamamos la función para lanzar el dado
+    await ctx.send(f"🎲 El resultado del dado fue: {resultado}")    
 
+bot.run(".")
